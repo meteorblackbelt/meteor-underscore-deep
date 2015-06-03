a23ff8c91189d8e7cec63d5d5b98a3f6af43529c
rubyblackbelt <rubyblackbelt@angelo.ca>
2015-06-03 22:07:03 +0000

    Initial commit

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  name: "meteorblackbelt:underscore-deep",
+  summary: "underscore-deep package",
+  version: "0.0.1",
+  git: "https://github.com/meteorblackbelt/meteor-underscore-deep",
+  documentation: null
+});
+
+
+Package.onUse(function (api) {
+  api.use(['coffeescript@1.0.6', 'underscore@1.0.3']);
+
+  api.addFiles('underscore-deep.coffee');
+});
